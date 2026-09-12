f364c14f2f63dd29c6bb7cec7f8aafd4b3b27c9b
Ákos Vaju <vaju.akos@gmail.com>
2026-09-12 21:55:09 +0200

    Organize files

---
+++ PackageFlow/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace PackageFlow;
+
+public partial class App : Application
+{
+    private static readonly IHost _host = Host
+        .CreateDefaultBuilder()
+        .ConfigureServices((context, services) =>
+        {
+            services.AddSingleton<MainWindow>();
+        })
+        .Build();
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _host.Start();
+
+        var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+
+        base.OnStartup(e);
+    }
+}
---
+++ PackageFlow/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Wpf.Ui.Controls;
+
+namespace PackageFlow;
+
+public partial class MainWindow : FluentWindow
+{
+    public MainWindow(IServiceProvider serviceProvider)
+    {
+        InitializeComponent();
+
+        RootNavigation.SetServiceProvider(serviceProvider);
+    }
+}
